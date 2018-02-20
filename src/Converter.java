c0e3ac9f084e4c17a07abb14b21a5215cc6fe3e4
Munal <dhakal.munal@gmail.com>
2018-02-20 12:14:41 +0545

    Size of the Converter is heaviely reduced

--- src/Converter.java
+++ src/Converter.java
@@ -6,13 +6,13 @@
  */
 public class Converter {
 
-	private static Long toConvert=Long.parseLong("100");
+	private static Long toConvert=Long.parseLong("556154535156");
+	private static String toString=String.valueOf(toConvert).trim();
+	private static String tester="test";
+	private static String outputValue="";
 	public static void main(String[] args) {
-		String toString=String.valueOf(toConvert).trim();
 		System.out.println(toString);
 		int count=toString.length();
-		String outputValue="";
-		String tester="test";
 		if(count==1&&toString.charAt(0)=='0') {
 			outputValue="सुन्ना";
 			toString="";
@@ -22,227 +22,12 @@ public class Converter {
 				outputValue=outputValue.concat("Number too large");
 				break;
 			}
-			if(count==13||count==12) {
-				if(toString.charAt(i)!='0') {
-					if(count==13) {
-							System.out.println("Hello");
-					//	if(toString.charAt(i+1)!='0') {
-							String a=Character.toString(toString.charAt(i));
-							a=a.concat(Character.toString(toString.charAt(i+1)));
-							String number=findNumber(Integer.parseInt(a));
-							number=number.concat(" ");
-							number=number.concat("खरब ");
-							outputValue=outputValue.concat(number);
-							tester="a";
-							count--;
-							i++;
-					//	}
-						/*else {
-							String number=findNumber(Integer.parseInt(Character.toString(toString.charAt(i))));
-							number.concat("");
-							number.concat("खरब");
-							outputValue.concat(number);
-						}*/
-					}
-					if(count==12) {
-						if(tester=="a") {
-							tester="test";
-						}
-						else {
-							System.out.println("Hello"+tester);
-							String number=findNumber(Integer.parseInt(Character.toString(toString.charAt(i))));
-							number.concat("");
-							number.concat("खरब");
-							outputValue.concat(number);
-						}
-					}
-					
-				}			
-			}
-			if(count==11||count==10) {
-				if(toString.charAt(i)!='0') {
-					if(count==11) {
-						//if(toString.charAt(i+1)!='0') {
-							String a=Character.toString(toString.charAt(i));
-							a=a.concat(Character.toString(toString.charAt(i+1)));
-							String number=findNumber(Integer.parseInt(a));
-							number=number.concat(" ");
-							number=number.concat("अरब ");
-							outputValue=outputValue.concat(number);
-							tester="a";
-							count--;
-							i++;
-						/*}
-						else {
-							String number=findNumber(Integer.parseInt(Character.toString(toString.charAt(i))));
-							number.concat("");
-							number.concat("अरब");
-							outputValue.concat(number);
-						}*/
-					}
-					if(count==10) {
-						if(tester=="a") {
-							tester="test";
-						}
-						else {
-							System.out.println("Hello"+tester);
-							String number=findNumber(Integer.parseInt(Character.toString(toString.charAt(i))));
-							number.concat("");
-							number.concat("अरब");
-							outputValue.concat(number);
-						}
-					}
-					
-				}		
-			}
-			if(count==9||count==8) {
-				if(toString.charAt(i)!='0') {
-					if(count==9) {
-						//if(toString.charAt(i+1)!='0') {
-							String a=Character.toString(toString.charAt(i));
-							a=a.concat(Character.toString(toString.charAt(i+1)));
-							String number=findNumber(Integer.parseInt(a));
-							number=number.concat(" ");
-							number=number.concat("करोड़ ");
-							outputValue=outputValue.concat(number);
-							tester="a";
-							count--;
-							i++;
-						/*}
-						else {
-							String number=findNumber(Integer.parseInt(Character.toString(toString.charAt(i))));
-							number.concat("");
-							number.concat("करोड़");
-							outputValue.concat(number);
-						}*/
-					}
-					if(count==8) {
-						if(tester=="a") {
-							tester="test";
-						}
-						else {
-							System.out.println("Hello"+tester);
-							String number=findNumber(Integer.parseInt(Character.toString(toString.charAt(i))));
-							number.concat("");
-							number.concat("करोड़");
-							outputValue.concat(number);
-						}
-					}
-					
-				}
-			}
-			if(count==7||count==6) {
-				if(toString.charAt(i)!='0') {
-					if(count==7) {
-						//if(toString.charAt(i+1)!='0') {
-							String a=Character.toString(toString.charAt(i));
-							a=a.concat(Character.toString(toString.charAt(i+1)));
-							String number=findNumber(Integer.parseInt(a));
-							number=number.concat(" ");
-							number=number.concat("लाख ");
-							outputValue=outputValue.concat(number);
-							tester="a";
-							count--;
-							i++;
-						/*}
-						else {
-							String number=findNumber(Integer.parseInt(Character.toString(toString.charAt(i))));
-							number.concat("");
-							number.concat("लाख");
-							outputValue.concat(number);
-						}*/
-					}
-					if(count==6) {
-						if(tester=="a") {
-							tester="test";
-						}
-						else {
-							System.out.println("Hello"+tester);
-							String number=findNumber(Integer.parseInt(Character.toString(toString.charAt(i))));
-							number.concat("");
-							number.concat("लाख");
-							outputValue.concat(number);
-						}
-					}
-					
-				}
-			}
-			if(count==5||count==4) {
-				if(toString.charAt(i)!='0') {
-					if(count==5) {
-						//if(toString.charAt(i+1)!='0') {
-							String a=Character.toString(toString.charAt(i));
-							a=a.concat(Character.toString(toString.charAt(i+1)));
-							String number=findNumber(Integer.parseInt(a));
-							number=number.concat(" ");
-							number=number.concat("हजार ");
-							outputValue=outputValue.concat(number);
-							tester="a";
-							count--;
-							i++;
-						/*}
-						else {
-							String number=findNumber(Integer.parseInt(Character.toString(toString.charAt(i))));
-							number.concat("");
-							number.concat("हजार");
-							outputValue.concat(number);
-						}*/
-					}
-					if(count==4) {
-						if(tester=="a") {
-							tester="test";
-						}
-						else {
-							String number=findNumber(Integer.parseInt(Character.toString(toString.charAt(i))));
-							number.concat("");
-							number.concat("हजार");
-							outputValue.concat(number);
-						}
-					}
-					
-				}
-			}
-			if(count==3) {
-				if(toString.charAt(i)!='0') {
-					String number=findNumber(Integer.parseInt(Character.toString(toString.charAt(i))));
-					number=number.concat(" ");
-					number=number.concat("सय ");
-					outputValue=outputValue.concat(number);
-				}
-			}
-			if(count==2||count==1) {
-				if(toString.charAt(i)!='0') {
-					if(count==2) {
-						//if(toString.charAt(i+1)!='0') {
-							String a=Character.toString(toString.charAt(i));
-							a=a.concat(Character.toString(toString.charAt(i+1)));
-							String number=findNumber(Integer.parseInt(a));
-							number=number.concat(" ");
-							number=number.concat("रुपैंया");
-							outputValue=outputValue.concat(number);
-							tester="";
-							count--;
-							i++;
-						//}
-					}
-					if(count==1) {
-						if(tester=="") {
-							tester="test";
-							continue;
-						}
-						else {
-							String number=findNumber(Integer.parseInt(Character.toString(toString.charAt(i))));
-							number.concat("");
-							number.concat("रुपैंया");
-							outputValue.concat(number);
-						}
-					}
-					
-				}
-			}
+			int x[]=setNumber(i,count);
+			count=x[0];
+			i=x[1];
 			count--;
 		}
-		System.out.println("Final Output:"+outputValue);
+		System.out.println("Final Output:"+outputValue+" रुपैंया");
 	}
 	public static String findNumber(int a) {
 		String[] nepaliValues={"", "एक", "दो", "तीन", "चार", "पाँच", "छह", "सात", "आठ", "नौ", "दस",
@@ -256,5 +41,41 @@ public class Converter {
 						"इक्यासी", "बयासी", "तिरासी", "चौरासी", "पचासी", "छियासी", "सत्तासी", "अट्ठासी", "नवासी", "नब्बे",
 						"इक्यानबे", "बानबे", "तिरानबे", "चौरानबे", "पंचानबे", "छियानबे", "सत्तानबे", "अट्ठानबे", "निन्यानबे"};
 		return nepaliValues[a];
+	}
+	public static String findPlace(int a) {
+		String [] place = {
+				" "," "," सय "," हजार "," हजार "," लाख "," लाख "," करोड़ "," करोड़ "," अरब "," अरब "," खरब "," खरब "
+		};
+		return place[a];
+	}
+	public static int[] setNumber(int i,int count) {
+		int x[]=new int[2];
+		if(toString.charAt(i)!='0') {
+			if(count==13||count==11||count==9||count==7||count==5||count==2) {
+				String a=Character.toString(toString.charAt(i)).concat(Character.toString(toString.charAt(i+1)));
+				outputValue=outputValue.concat(findNumber(Integer.parseInt(a))).concat(findPlace(count-1));
+				count--;
+				tester="";
+				i++;
+				x[0]=count;
+				x[1]=i;
+			}
+			if(count==1||count==4||count==6||count==8||count==10||count==12) {
+				if(tester=="") {
+					tester="test";
+				}
+				else {
+					outputValue=outputValue.concat(findNumber(Integer.parseInt(Character.toString(toString.charAt(i))))).concat(findPlace(count-1));
+					x[0]=count;
+					x[1]=i;
+				}
+			}
+			if(count==3) {
+				outputValue=outputValue.concat(findNumber(Integer.parseInt(Character.toString(toString.charAt(i))))).concat(findPlace(count-1));
+				x[0]=count;
+				x[1]=i;
+			}
+		}
+		return x;
 	}
 }
